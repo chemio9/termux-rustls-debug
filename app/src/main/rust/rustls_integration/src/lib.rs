6ba2f9996c9228723f62eae4284f443b328647f4
Clarence <chengruichen3@gmail.com>
2025-03-15 10:09:57 +0800

    更新 lib.rs

--- app/src/main/rust/rustls_integration/src/lib.rs
+++ app/src/main/rust/rustls_integration/src/lib.rs
@@ -4,6 +4,6 @@ use jni::objects::*;
 use jni::JNIEnv;
 use rustls_platform_verifier::android;
 #[export_name = "Java_com_termux_app_TermuxInstaller_rustls"]
-extern "C" fn java_init(env: JNIEnv, _class: JClass, context: JObject) {
+extern "C" fn java_init(mut env: JNIEnv, _class: JClass, context: JObject) {
     android::init_hosted(&mut env, context).unwrap();
 }
